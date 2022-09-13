4580d1d0600f8e3bae4a14fe392a1958eda2b706
Theodore Batik <theodore.batik@eighty20.co.za>
2022-09-13 19:56:47 +0200

    Clean up input script

--- src/input.py
+++ src/input.py
@@ -1,10 +1,10 @@
-# PURPOSE:
-    # Input data (stoichiometric matrices & measured concentrations)
-    # Clean data (equalize time spacing, remove zeros, etc)
+# PURPOSE OF THIS SCRIPT:
+    # Input data (stoichiometric matrices & the measured concentrations)
+    # Clean data (equalize time spacings, remove zeros, etc.)
     # Convert pH to [H+]
     # Normalize
     # Interpolate
-    # Output processed data
+    # Output prepared data (for validation)
 
 
 # IMPORTS 
@@ -17,20 +17,14 @@ from scipy import interpolate as interp
 
 # INPUT DATA
 
-# Set base directory
+# Set base directory and move to 'raw' data folder
 base_dir = os.getcwd()
-
-# Move to raw data folder
-
-# os.chdir('data')
-# data_dir = os.getcwd()
-
 os.chdir(os.path.join(base_dir, 'data', 'raw'))
 
 # Load the measured concentrations into a pandas dataframe
 c_measured = pd.read_csv('c_measured.csv')
 
-# Define function to load .csv file, normalize each row and return matrix
+# Define function to load .csv for the stoich. matrices and normalize each row
 def load_csv(file_name):
     matrix = []
     with open(f'{file_name}.csv', 'r') as file:
@@ -45,24 +39,20 @@ def load_csv(file_name):
     matrix = np.asarray(matrix)
     return matrix, header
 
+# Define the stoich. matrices A & B
 A, A_header = load_csv('A')
 B, B_header = load_csv('B')
 
-print('A = \n', A)
-print('A_header = \n', A_header)
-print('B = \n', B)
-print('B_header = \n', B_header)
-
-# Define the stoichiometric matrix, X = B - A
+# Define the stoich. matrix, X = B - A
 X = B - A
-X = X.transpose()
 # X = np.vstack((X, -X)) # stacks the stoic. coefficients of the reverse reactions (-X) onto that of the forward reactions (X)
-# A = np.vstack((X, -X)) ... and B
+X = X.transpose() # such that # columns = # reactions & # rows = # species
+# A = np.vstack((A, -A))
 
 
-# CLEANING and pH CONVERSION
+# DATA CLEANING and pH CONVERSION
 
-# Set number of rows
+# Number of rows in the dataframe of raw measurements
 n = len(c_measured)
 
 #  Turn all zeros (missing raw data) into NaN's, for IC in list of Interfering Compounds
@@ -79,16 +69,14 @@ for ree in columns:
     for i in range(n):
         c_measured.loc[i, [ree]] *= 0.001 
 
-# Add empty column for to populate with [H+]
+# Add empty column to be populated with [H+]
 empty_column = np.zeros(n)
 c_measured['H+'] = empty_column
 
-# Convert each pH to [H+]
+# Convert each pH to [H+], then drop pH column
 for i in range(n):
     pH = c_measured.loc[i, 'pH']
     c_measured.loc[i, 'H+'] = 10**(-pH)
-
-# Drop pH column
 c_measured = c_measured.drop(['pH'], axis=1)
 
 # Create an empty template like c_measured - to be altered & populated
@@ -122,10 +110,9 @@ for i in range(n):
 #     c_prepared[column] = min_max_scaling(c_prepared[column])
 
 
-
 # INTERPOLATION
 
-# Define the independent variable - days
+# Define the independent variable, 'days'
 N = len(c_prepared)
 days = c_prepared.loc[:, ['Day']].values
 days = np.asarray(days[:, 0])
@@ -149,27 +136,25 @@ def interp_column(dataframe, column_name, days):
     else:
         return False
 
-# Interpolate on each column, if possible. Store list of NaN columns.
+# Interpolate on each column, if possible. Store list of NaN-columns.
 column_names = c_prepared.columns
 nan_columns = []
-for column_name in column_names: #.remove('Days'):
+for column_name in column_names:
     y_interp = interp_column(c_prepared, column_name, days)
-    if y_interp:
+    if y_interp: # if interp. possible, i.e. not a column of NaN's
         for i in range(N):
             value = c_prepared.loc[i, [column_name]].values[0]
             if np.isnan(value):
                 c_prepared.loc[i, column_name] = y_interp(i)
     else:
         nan_columns.append(column_name)
-# print('NaN Columns', nan_columns)
 
 # Add Water, 1 mg/L? and 10 L => 10 mg
 
 
 # OUTPUT PREPARED DATA
 
-# Move to prepared data folder
-
+# Move to 'prepared' data folder (create it if non-existent)
 os.chdir(os.path.join(base_dir, 'data'))
 os.makedirs('prepared', exist_ok=True)
 os.chdir('prepared')
@@ -180,13 +165,14 @@ A.tofile('A.csv',sep=',')
 B.tofile('B.csv',sep=',')
 X.tofile('X.csv',sep=',')
 
-# Drop 'Day' column (not a state of system)
+# Drop 'Day' column (not a state of the system)
+days = c_prepared.loc[:, ['Day']].values
 c_prepared_no_days = c_prepared.drop(['Day'], axis=1)
 
 # Final input into objective function
 states_measured = c_prepared_no_days.to_numpy()
 
-# Go back to parent
+# Go back to repo parent folder
 os.chdir(base_dir)
 
 
