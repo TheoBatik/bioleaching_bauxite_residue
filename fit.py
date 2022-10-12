6817885bc112d69e9c8f4d6a9d899a53a6049f50
Theodore Batik <theodore.batik@eighty20.co.za>
2022-10-12 09:38:32 +0200

    Make evolve_net_method explicitly changable

--- fit.py
+++ fit.py
@@ -17,13 +17,18 @@ kinf.set_initial_conditions()
 kinf.set_evalutation_times()
 
 # OPTIMISATION
+kinf.evolve_net_method = 'Radau' # 'BDF' # 'LSODA' , 
 n_epochs = 1
-iters_per_epoch = 2
+iters_per_epoch = 25
 k_expo_0 = kinf.fetch_random_k_expo()
 kinf.prepare_data_for_optimisation()
-kinf.fit_kinetics_by_basinhopping( n_epochs, iters_per_epoch, k_expo_0, display=True )
+kinf.fit_kinetics_by_basin_hopping( n_epochs, iters_per_epoch, k_expo_0, display=True )
 
 # RESULTS
+kinf.plot_objective_by_iteration()
+kinf.plot_kinetics_by_iteration()
+kinf.plot_predicted_vs_measured()
+
 print(kinf.best_prediction)
 
 attrs = vars(kinf)
