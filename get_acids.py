fb07f2593ff2df34bcbaee8ab48f44bfc019ba15
Theodore Batik <theodore.batik@eighty20.co.za>
2023-02-15 14:01:47 +0200

    Add optimise function to acid production model

--- get_acids.py
+++ get_acids.py
@@ -13,38 +13,40 @@ print('Measured states\n', a.states_m)
 print('Evaluation times\n', a.eval_times)
 
 
-# # Set initial conditions
+# # Set initial conditions / params
 x0 = 4 # mg/L
 s0 = 125000#625000 # [substrate] at t=0 (sucrose) mg/L
 p0 = [ a.states_m[0, i] for i in range(0,3) ] # mg/L
 f0 = [x0, s0, *p0]
 a.set_initial_conditions( f0 )
 print( 'Initial conditions\n', a.f0 )
-
-
-# # Test solution with random model params
 params_0 = ( 0.5, 0.01704, 1, 0.2, 0.2, 0.4 )
-f = a.solve_system( params_0 ).y
-print(f.shape)
+
+# # Test solution with initial model params
+# f = a.solve_system( params_0 ).y
+# print(f)
+
+cost = a.cost( params_0 )
+# print( 'Cost', cost )
 
 # f_model = a.cost( params_0 )
 # print( 'f_model', f_model )
+
+optimal_params = a.optimise( params_0, display=True )
+print( 'Optimal model parameters', optimal_params )
+f_optimal = a.solve_system( optimal_params ).y
 
 
 # Fit solution to measured data
 # popt, pcov = a.fit( params_0 )
 
-
-
-
 import matplotlib.pyplot as plt
 t = a.eval_times
 print(t.shape)
 
-
 plt.figure()
 for i in range(0, 5):
-    plt.plot( t, f[i, :] )
-# for i in range(0,3):
-#     plt.plot( a.eval_times, a.states_m[:, i])
+    plt.plot( t, f_optimal[i, :] )
+for i in range(0,3):
+    plt.plot( a.eval_times, a.states_m[:, i])
 plt.show()
--- src/acid_production.py
+++ src/acid_production.py
@@ -2,7 +2,8 @@
 
 
 # Imports
-from scipy.integrate import odeint, solve_ivp
+from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
 # from scipy.optimize import curve_fit
 import numpy as np
 
@@ -14,8 +15,6 @@ q = 2.17680
 r = 0.29370
 
 # Initial guess of fixable parameters
-
-
 class AcidAniger:
     
     def input( self, measurements ):
@@ -25,8 +24,14 @@ class AcidAniger:
         setattr( self, 'species_m', measurements[1][1:] ) # measured species
         setattr( self, 'eval_times', measurements[0][:, 0] ) # times at which to evaluate the soln
 
+        # Set maxium of the measured states
+        setattr( self, 'max_measured', np.max( self.states_m ) )
 
-    def ode_system( self, t, f, args ):
+        # Normlise the measured states
+        setattr( self, 'states_nm', self.states_m / self.max_measured )
+
+
+    def ode_system( self, t, f, *args ):
         ''' 
         System of differential equations for:
         1) Biomass production, x (Monod dynamics assumed)
@@ -57,42 +62,18 @@ class AcidAniger:
     def set_initial_conditions( self, f0 ):
         setattr( self, 'f0', f0 )
 
-    # Using odeint
-    def solve_full_system( self, t, *args ):
-        '''
-        Solves the ODE system for all variables 
-        given the model parameters
-        '''
-        # if isinstance( t, float ) or isinstance( t, int ):
-        #     t = [t]
-        
-        f_full = odeint(
-            self.ode_system,
-            self.f0,
-            t,
-            args=args,
-            rtol=1e-13,
-            atol=1e-12
-        )
-
-        return f_full
 
     # Using solve_ivp
-    def solve_system( self, *params, t_eval=None ):
+    def solve_system( self, params, t_eval=None ):
         '''
         Solves the ODE system for all variables 
         given the model parameters
         '''
-        # if isinstance( t, float ) or isinstance( t, int ):
-        #     t = [t]
         
         if t_eval is None:
             t_eval = self.eval_times
 
-        print( 'Eval times' , t_eval )
-        print( 't span', [t_eval[1], t_eval[-1]]  )
-
-        sol = solve_ivp(
+        f = solve_ivp(
             self.ode_system, # system to solve
             [t_eval[0], t_eval[-1]], # time span
             self.f0, # initial conditions
@@ -100,60 +81,32 @@ class AcidAniger:
             t_eval=t_eval,
             args=params, # parameters
             dense_output=False
-        )
+        ).y
 
-        return sol
-    
-    
-    def set_times( self ):
-        
-        t_filler = np.logspace( 
-            -3 , 
-            np.log2( int(self.eval_times[-1] )), 
-            num=150, 
-            base=2
-        )
-
-        t_all = sorted( 
-                np.concatenate((
-                    t_filler, 
-                    self.eval_times
-                )) 
-            )
-
-        indices = []
-        for i, t in enumerate(t_all):
-            if t in self.eval_times:
-                j = self.eval_times.index( t )
-                indices.append( j )
-
-        return indices
+        return f
         
 
     def cost( self, params ):
         
-        t_filler = np.logspace( 
-            -3 , 
-            np.log2( int(self.eval_times[-1] )), 
-            num=150, 
-            base=2
-        )
+        states_p = np.transpose( self.solve_system( params ) ) # predicted states
+        states_nvp = np.delete( states_p, [0, 1], 1 ) / self.max_measured # normalised visible predicted states 
+        del states_p
 
-        t = sorted( 
-                np.concatenate((
-                    t_filler, 
-                    self.eval_times
-                )) 
-            )
+        # Calculate the net error: sum (over time) of the discrepencies squared
+        discrepency = states_nvp[1:, :] - self.states_nm[1:, :]
+        net_error = np.sum( np.multiply( discrepency, discrepency ) )
 
-        f_full = self.solve_full_system( t, params )
-        f_reduced = np.delete( f_full, [0, 1], 1 )
+        return net_error 
 
 
+    def optimise( self, params_0, display=False ):
 
-        return f_reduced
+        optimal_params = minimize(self.cost, params_0, method='L-BFGS-B' ) #, tol=1e-6)
+        
+        if display:
+            print( 'Cost function value at:', self.cost( optimal_params ) )
 
-
+        return optimal_params
 
 
 
